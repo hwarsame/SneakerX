7c3579feee6af7517af738d0a1954da1b7d83d3d
Hamza Warsame <hamza360123@gmail.com>
2023-08-28 21:21:21 -0400

    complete base sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@
     <section className="padding-x py-10">Services</section>
     <section className="padding">SpecialOffers</section>
     <section className="bg-pale-blue padding">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16">Subscribe</section>
-    <section className="padding">Footer</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
    </main>
   )
 
