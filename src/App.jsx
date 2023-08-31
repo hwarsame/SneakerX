f5ca553a744cf27c1c891ff8372b1aa391944b87
Hamza Warsame <hamza360123@gmail.com>
2023-08-30 20:39:08 -0400

    Create NavBar and Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
- const App = () => 
-  (
-   <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
-    </section>
-    <section className="padding">PopularProduct</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffers</section>
-    <section className="bg-pale-blue padding">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
-   </main>
-  )
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  Services,
+  PopularProduct,
+  SpecialOffers,
+  SuperQuality,
+  Subscribe,
+} from "./sections";
+import Nav from "./components/Nav";
 
-export default App; 
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProduct />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
