1b51c0e1c156d11f9d3f4834aac95ae38da835d8
Hamza Warsame <hamza360123@gmail.com>
2023-09-14 17:29:06 -0400

    create popularProductCard and add imgs

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,12 @@
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default PopularProductCard;
