6e84d28565d96243666288e5da2cb33d736bb436
Hamza Warsame <hamza360123@gmail.com>
2023-09-14 15:14:30 -0400

    integrate shoeCard into Hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import ShoeCard from "../components/ShoeCard";
 import { arrowRight } from "../assets/icons";
@@ -5,6 +6,8 @@ import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
+  const [bigShoeImg, setbigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
@@ -42,19 +45,19 @@ const Hero = () => {
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center ">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard
                 imgURL={shoe}
-                changeBigShoeImage={() => {}}
-                bigShoeImg=""
+                changeBigShoeImage={(shoe) => setbigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
               />
             </div>
           ))}
